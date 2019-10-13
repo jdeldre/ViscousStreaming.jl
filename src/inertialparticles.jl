698d9fa76cc2dab1afa8d3c781c1904f1b383ab1
Jeff Eldredge <jdeldre@g.ucla.edu>
2019-10-12 20:47:50 -0700

    clarified reference to HistoryType

--- src/inertialparticles.jl
+++ src/inertialparticles.jl
@@ -40,7 +40,7 @@ function inertial_velocity(ux::History{S,H},uy::History{T,H},
         duxdt::History{S,H},duydt::History{T,H},
         w::History{R,H},
         g::PhysicalGrid,Δt::Real,p::InertialParameters;cflag::Bool=false) where
-          {S<:XEdges,T<:YEdges,R<:Nodes,H<:HistoryType}
+          {S<:XEdges,T<:YEdges,R<:Nodes,H<:Systems.HistoryType}
 
     # vx, vy serve as temp storage for du/dt
     vx = deepcopy(duxdt)
