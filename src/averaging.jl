fec8219421ffb3ae2647f67d997e544406a7c02c
Jeff Eldredge <jdeldre@g.ucla.edu>
2019-10-12 23:24:38 -0700

    added Reynolds decomp and interpolation routines

--- src/averaging.jl
+++ src/averaging.jl
@@ -1,2 +1,20 @@
 ## Here, define routines that
-#  - 
+#  -
+
+export reynolds_decomposition!
+
+"""
+    reynolds_decomposition!(q̄::Edges,q̃x::Vector{XEdges},q̃y::Vector{YEdges},itr)
+
+Perform a Reynolds decomposition of the vectors of velocity component histories `q̃x` and `q̃y`. Both
+components of the mean are placed in `q̄`, and the original vectors of component histories are
+overwritten with the fluctuations from the mean. The mean is computed over the range `itr`.
+"""
+function reynolds_decomposition!(q̄::Edges,q̃x::History{S},q̃y::History{T},itr) where {S<:XEdges, T<:YEdges}
+    q̄.u .= mean(q̃x[itr])
+    q̄.v .= mean(q̃y[itr]);
+    q̃x .= map(q -> q - q̄.u,q̃x)
+    q̃y .= map(q -> q - q̄.v,q̃y)
+
+    return q̄, q̃x, q̃y
+end
--- src/displacement.jl
+++ src/displacement.jl
@@ -1,1 +1,21 @@
 # routines for computing the disturbed displacement field
+
+using Interpolations
+
+export interpolated_history
+
+"""
+    interpolated_history(v::History,tr)
+
+Returns a function that interpolates the history `v`, using the time specified
+by time range `tr`. Uses cubic spline interpolation. If `v` is a periodic history,
+then the resulting function is also periodic.
+"""
+function interpolated_history(v::History{T,PeriodicHistory},tr::AbstractRange) where {T}
+  return CubicSplineInterpolation(tr,Base.unsafe_view(v,1:length(tr)),extrapolation_bc=Periodic())
+end
+
+function interpolated_history(v::History{T,RegularHistory},tr::AbstractRange) where {T}
+  length(tr) == length(v) || error("Incommensurate lengths of history and range")
+  return CubicSplineInterpolation(tr,v,extrapolation_bc=Periodic())
+end
