d47ec1d1d81b288b4590acdfac240bb7c73043ea
Jeff Eldredge <jdeldre@g.ucla.edu>
2019-10-13 17:00:17 -0700

    Updated with trajectory routines in module

--- src/trajectories.jl
+++ src/trajectories.jl
@@ -5,13 +5,13 @@ using DiffEqCallbacks # for manifold projection
 
 export compute_trajectory
 
-function compute_trajectory(vfcn,X₀::Tuple,Tmax::Real,Δt::Real;bl::BodyList=BodyList())
+function compute_trajectory(vfcn,X₀::Tuple,Tmax::Real,Δt::Real;bl::BodyList=BodyList(),ϵ::Real=0.0)
 
   u0 = [X₀[1],X₀[2]]
   tspan=(0.0,Tmax)
 
   if length(bl) > 0
-    indicator!(resid,u,p,t) = body_indicator!(resid,u,p,t,bl)
+    indicator!(resid,u,p,t) = body_indicator!(resid,u,p,t,ϵ,bl)
     cbdomain = ManifoldProjection(indicator!)
     Path = ODEProblem(vfcn,u0,tspan,callback=cbdomain)
     sol = solve(Path,ABM54(), dt = Δt, maxiters = 1e8, adaptive = false, dense = true, progressbar = true);
@@ -23,23 +23,23 @@ function compute_trajectory(vfcn,X₀::Tuple,Tmax::Real,Δt::Real;bl::BodyList=B
   return sol
 end
 
-compute_trajectory(vfcn,X₀::Tuple,Tmax::Real,Δt::Real,body::Body) =
-      compute_trajectory(vfcn,X₀,Tmax,Δt;bl=BodyList([body]))
+compute_trajectory(vfcn,X₀::Tuple,Tmax::Real,Δt::Real,body::Body;ϵ::Real=0.0) =
+      compute_trajectory(vfcn,X₀,Tmax,Δt;bl=BodyList([body]),ϵ=ϵ)
 
 
 # This is the function that is constrained to be zero by manifold projection
-function body_indicator!(resid,u,p,t,bl)
+function body_indicator!(resid,u,p,t,ϵ,bl)
   # this only works for circles now
   maxϕ = -1.0e10
   for b in bl
-    maxϕ = max(maxϕ,ϕ(u,b.cent,b.a))
+    maxϕ = max(maxϕ,ϕ(u,[b.cent[1],b.cent[2]],b.a))
   end
   resid[1] = Hs(ϵ)(maxϕ)
   resid[2] = 0.0
 end
 
 #
-function ϕ(x::Vector,xc::Vector,R::Real)
+function ϕ(x,xc::Vector,R::Real)
     # Returns a value that is positive if x is inside a circle of radius R
     # or negative if x is outside the circle
     out = R - norm(x-xc)
