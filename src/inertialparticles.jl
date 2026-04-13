db22910e905591938f6a688511c3cd7255152c8a
qiyuanbillwu <billwu2003@gmail.com>
2026-04-13 14:08:32 -0700

    regularization for w = 0 at boundary when computing saffman term

--- src/inertialparticles.jl
+++ src/inertialparticles.jl
@@ -3,7 +3,7 @@
 
 import ImmersedLayers: laplacian!
 
-export InertialParameters,inertial_velocity, saffman, _unscaled_convective_derivative!
+export InertialParameters,inertial_velocity, saffman, _unscaled_convective_derivative!, acc1, acc2
 
 """
     InertialParameters
@@ -296,54 +296,58 @@ The result (the 0th and 2nd-order Fourier coefficients) are returned as primal e
 """
 function saffman(u::Edges{Primal,NX,NY,ComplexF64},ω::Nodes{Dual,NX,NY,ComplexF64}) where {NX,NY}
 
-  J∞ = 2.255
-  Ω = 1.0
+    J∞ = 2.255
+    Ω = 1.0
 
-  Ĉ0 = _coefficient(0)
-  Ĉ2 = _coefficient(2)
+    Ĉ0 = _coefficient(0)
+    Ĉ2 = _coefficient(2)
 
-  expiϕ = similar(ω)
-  expiϕ .= ω./abs(ω)
+    absω = abs.(ω)
+    ω_floor = 1e-10   # tune this
+    safe_absω = max.(absω, ω_floor)
 
-  b0_node = similar(ω)
-  b2_node = similar(ω)
+    expiϕ = similar(ω)
+    expiϕ .= ω./safe_absω
 
-  b0_node .= Ĉ0
-  b2_node .= Ĉ2*(expiϕ∘expiϕ)
+    b0_node = similar(ω)
+    b2_node = similar(ω)
 
-  b0_node ./= sqrt.(abs(ω))
-  b2_node ./= sqrt.(abs(ω))
+    b0_node .= Ĉ0
+    b2_node .= Ĉ2*(expiϕ∘expiϕ)
 
-  b0 = similar(u)
-  b2 = similar(u)
+    b0_node ./= sqrt.(safe_absω)
+    b2_node ./= sqrt.(safe_absω)
 
-  grid_interpolate!(b0.u,b0_node)  # primal x edges
-  grid_interpolate!(b0.v,b0_node)
-  grid_interpolate!(b2.u,b2_node)  # primal y edges
-  grid_interpolate!(b2.v,b2_node)
+    b0 = similar(u)
+    b2 = similar(u)
 
-  a0 = similar(u)
-  a2 = similar(u)
+    grid_interpolate!(b0.u,b0_node)  # primal x edges
+    grid_interpolate!(b0.v,b0_node)
+    grid_interpolate!(b2.u,b2_node)  # primal y edges
+    grid_interpolate!(b2.v,b2_node)
 
-  uxnode = Nodes(Dual,ω,dtype=ComplexF64)
-  uynode = Nodes(Dual,ω,dtype=ComplexF64)
-  grid_interpolate!(uynode, u.v)
-  grid_interpolate!(uxnode,-u.u)
+    a0 = similar(u)
+    a2 = similar(u)
 
-  grid_interpolate!(a0.u, uynode ∘ conj(ω))
-  grid_interpolate!(a0.v, uxnode ∘ conj(ω))
-  grid_interpolate!(a2.u, uynode ∘ ω)
-  grid_interpolate!(a2.v, uxnode ∘ ω)
+    uxnode = Nodes(Dual,ω,dtype=ComplexF64)
+    uynode = Nodes(Dual,ω,dtype=ComplexF64)
+    grid_interpolate!(uynode, u.v)
+    grid_interpolate!(uxnode,-u.u)
 
-  Ls0 = 0.5*(a0 ∘ b0 + conj(a0) ∘ b0 + conj(a2) ∘ b2)
-  Ls2 = 0.5*(a0 ∘ b2 + conj(a0) ∘ b2 + b0 ∘ a2 + conj(b0) ∘ a2)
+    grid_interpolate!(a0.u, uynode ∘ conj(ω))
+    grid_interpolate!(a0.v, uxnode ∘ conj(ω))
+    grid_interpolate!(a2.u, uynode ∘ ω)
+    grid_interpolate!(a2.v, uxnode ∘ ω)
 
-  K = 3sqrt(3)/(2π^2)*J∞/Ω
+    Ls0 = 0.5*(a0 ∘ b0 + conj(a0) ∘ b0 + conj(a2) ∘ b2)
+    Ls2 = 0.5*(a0 ∘ b2 + conj(a0) ∘ b2 + b0 ∘ a2 + conj(b0) ∘ a2)
 
-  Ls0 .*= K
-  Ls2 .*= K
+    K = 3sqrt(3)/(2π^2)*J∞/Ω
 
-  return Ls0, Ls2
+    Ls0 .*= K
+    Ls2 .*= K
+
+    return Ls0, Ls2
 
 end
 
