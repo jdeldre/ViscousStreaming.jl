f861151f54247df63e48b1e83964c3f229bc4c7e
qiyuanbillwu <billwu2003@gmail.com>
2026-04-13 20:37:33 -0700

    add debug option to saffman function

--- src/inertialparticles.jl
+++ src/inertialparticles.jl
@@ -5,7 +5,10 @@ using Interpolations
 
 import ImmersedLayers: laplacian!
 
-export InertialParameters,inertial_velocity, saffman, _unscaled_convective_derivative!, ParticleFlow
+export InertialParameters,inertial_velocity, _unscaled_convective_derivative!, ParticleFlow
+
+# for testing only
+export acc1, acc2, saffman
 
 """
     InertialParameters
@@ -302,14 +305,17 @@ struct ParticleFlowField
 end
 
 """
-    saffman(u::Edges{Primal,NX,NY,ComplexF64},ω::Nodes{DualNX,NY,ComplexF64})
+    saffman(u, ω; return_debug=false, ω_floor=1e-10)
 
-Computes the Saffman lift operator ``\\mathcal{L}_s``, using velocity field `u` (in primal edge data)
-and vorticity field `ω` (in dual node data), given as complex amplitudes for oscillatory
-solution. Note that `ω` should be the proper vorticity (i.e., scaled by the grid spacing).
-The result (the 0th and 2nd-order Fourier coefficients) are returned as primal edge data
+Computes the Saffman lift operator. If `return_debug=true`, also returns a NamedTuple
+of intermediate quantities for debugging/comparison.
 """
-function saffman(u::Edges{Primal,NX,NY,ComplexF64},ω::Nodes{Dual,NX,NY,ComplexF64}) where {NX,NY}
+function saffman(
+    u::Edges{Primal,NX,NY,ComplexF64},
+    ω::Nodes{Dual,NX,NY,ComplexF64};
+    return_debug::Bool=false,
+    ω_floor::Float64=1e-10
+) where {NX,NY}
 
     J∞ = 2.255
     Ω = 1.0
@@ -317,18 +323,20 @@ function saffman(u::Edges{Primal,NX,NY,ComplexF64},ω::Nodes{Dual,NX,NY,ComplexF
     Ĉ0 = _coefficient(0)
     Ĉ2 = _coefficient(2)
 
-    absω = abs.(ω)
-    ω_floor = 1e-10   # tune this
-    safe_absω = max.(absω, ω_floor)
+    absω = similar(ω, Float64)
+    absω .= abs.(ω)
+
+    safe_absω = similar(ω, Float64)
+    safe_absω .= max.(absω, ω_floor)
 
     expiϕ = similar(ω)
-    expiϕ .= ω./safe_absω
+    expiϕ .= ω ./ safe_absω
 
     b0_node = similar(ω)
     b2_node = similar(ω)
 
     b0_node .= Ĉ0
-    b2_node .= Ĉ2*(expiϕ∘expiϕ)
+    b2_node .= Ĉ2 * (expiϕ ∘ expiϕ)
 
     b0_node ./= sqrt.(safe_absω)
     b2_node ./= sqrt.(safe_absω)
@@ -336,34 +344,69 @@ function saffman(u::Edges{Primal,NX,NY,ComplexF64},ω::Nodes{Dual,NX,NY,ComplexF
     b0 = similar(u)
     b2 = similar(u)
 
-    grid_interpolate!(b0.u,b0_node)  # primal x edges
-    grid_interpolate!(b0.v,b0_node)
-    grid_interpolate!(b2.u,b2_node)  # primal y edges
-    grid_interpolate!(b2.v,b2_node)
+    grid_interpolate!(b0.u, b0_node)
+    grid_interpolate!(b0.v, b0_node)
+    grid_interpolate!(b2.u, b2_node)
+    grid_interpolate!(b2.v, b2_node)
 
     a0 = similar(u)
     a2 = similar(u)
 
-    uxnode = Nodes(Dual,ω,dtype=ComplexF64)
-    uynode = Nodes(Dual,ω,dtype=ComplexF64)
+    uxnode = Nodes(Dual, ω, dtype=ComplexF64)
+    uynode = Nodes(Dual, ω, dtype=ComplexF64)
     grid_interpolate!(uynode, u.v)
-    grid_interpolate!(uxnode,-u.u)
+    grid_interpolate!(uxnode, -u.u)
 
-    grid_interpolate!(a0.u, uynode ∘ conj(ω))
-    grid_interpolate!(a0.v, uxnode ∘ conj(ω))
-    grid_interpolate!(a2.u, uynode ∘ ω)
-    grid_interpolate!(a2.v, uxnode ∘ ω)
+    a0_node_u = similar(ω)
+    a0_node_v = similar(ω)
+    a2_node_u = similar(ω)
+    a2_node_v = similar(ω)
 
-    Ls0 = 0.5*(a0 ∘ b0 + conj(a0) ∘ b0 + conj(a2) ∘ b2)
-    Ls2 = 0.5*(a0 ∘ b2 + conj(a0) ∘ b2 + b0 ∘ a2 + conj(b0) ∘ a2)
+    a0_node_u .= uynode ∘ conj(ω)
+    a0_node_v .= uxnode ∘ conj(ω)
+    a2_node_u .= uynode ∘ ω
+    a2_node_v .= uxnode ∘ ω
 
-    K = 3sqrt(3)/(2π^2)*J∞/Ω
+    grid_interpolate!(a0.u, a0_node_u)
+    grid_interpolate!(a0.v, a0_node_v)
+    grid_interpolate!(a2.u, a2_node_u)
+    grid_interpolate!(a2.v, a2_node_v)
 
+    Ls0 = 0.5 * (a0 ∘ b0 + conj(a0) ∘ b0 + conj(a2) ∘ b2)
+    Ls2 = 0.5 * (a0 ∘ b2 + conj(a0) ∘ b2 + b0 ∘ a2 + conj(b0) ∘ a2)
+
+    K = 3sqrt(3) / (2π^2) * J∞ / Ω
     Ls0 .*= K
     Ls2 .*= K
 
-    return Ls0, Ls2
-
+    if return_debug
+        debug = (
+            Ĉ0 = Ĉ0,
+            Ĉ2 = Ĉ2,
+            ω = deepcopy(ω),
+            absω = deepcopy(absω),
+            safe_absω = deepcopy(safe_absω),
+            expiϕ = deepcopy(expiϕ),
+            b0_node = deepcopy(b0_node),
+            b2_node = deepcopy(b2_node),
+            b0 = deepcopy(b0),
+            b2 = deepcopy(b2),
+            uxnode = deepcopy(uxnode),
+            uynode = deepcopy(uynode),
+            a0_node_u = deepcopy(a0_node_u),
+            a0_node_v = deepcopy(a0_node_v),
+            a2_node_u = deepcopy(a2_node_u),
+            a2_node_v = deepcopy(a2_node_v),
+            a0 = deepcopy(a0),
+            a2 = deepcopy(a2),
+            Ls0 = deepcopy(Ls0),
+            Ls2 = deepcopy(Ls2),
+            K = K,
+        )
+        return Ls0, Ls2, debug
+    else
+        return Ls0, Ls2
+    end
 end
 
 _coefficient(n) = 2^(1/2)*(ellipk(1/2)/π)*gamma(n/2+1/4)^2/gamma(1/4)^2/gamma(n+1/2)*sqrt(π)*2^n*(-1)^(n/2)
